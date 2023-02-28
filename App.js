2583392af7b64eeeb0eeccadf506fb03b80dd5b4
nance1798 <nance1798@students.d211.org>
2023-02-28 09:19:39 -0600

    doing stuff

--- App.js
+++ App.js
@@ -4,11 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to startttt working on your app! </Text>
+      <Text>Open up App.js to startttt working on your appp! </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+// I'm commenting
 //test comment
 const styles = StyleSheet.create({
   container: {
